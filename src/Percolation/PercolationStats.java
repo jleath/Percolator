12fc0a65358092652abe8d849f6eca9c0652e2d6
Joshua Leath <lawnfurniture@icloud.com>
2016-10-07 09:26:22 -0400

    Update PercolationStats.java

--- src/Percolation/PercolationStats.java
+++ src/Percolation/PercolationStats.java
@@ -1,6 +1,3 @@
-package Percolation;
-
-import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
@@ -11,35 +8,33 @@ import edu.princeton.cs.algs4.StdStats;
  * a nice representation of the system if desired.
  */
 public class PercolationStats {
-    /** The maximum number of operations that a test can fail before it will give up and restart. */
-    private static final int MAX_FAILED_OPERATIONS = 50;
-    /** The amount of time in milliseconds to pause the display upon percolation. */
-    private static final int DISPLAY_PAUSE_ON_SUCCESS = 2000;
-    /** The default amount of time to delay the display between each operation. */
-    private static final int DEFAULT_DELAY_PAUSE = 0;
     /** The scaling factor for calculating the confidence interval. */
     private static final double CONFIDENCE_SCALING_FACTOR = 1.96;
 
     /** An array to store the percolation threshold of each test. */
-    private int[] thresholds;
+    private double[] thresholds;
     /** The number of tests to run. */
     private int T;
     /** The size of the grid to be used for tests. */
     private int N;
-    /** True if the user used the -show flag upon execution. */
-    private boolean display;
-    /** An object to track the inputs that led to a test failure. */
-    private FailureSequence failureTracker;
-    /** A count of the number of failed tests. */
-    private int failures;
+    
+    public static void Main(String[] args) {
+        if (args.length < 2) {
+            System.out.printf("Invalid arguments:\nUsage:\n\tjava StatsTest [grid size] [number of tests] (-show)\n");
+        }
+        int N = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
+        PercolationStats ps = new PercolationStats(N, T);
+        System.out.printf("mean = %f\nstddev = %f\n95%% confidence interval = %f, %f\n", ps.mean(), ps.stddev(), ps.confidenceLo(), ps.confidenceHi());
+    }
 
-    public PercolationStats(int N, int T, boolean display) {
-        thresholds = new int[T];
+    public PercolationStats(int N, int T) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("Invalid args to PercolationStats Constructor");   
+        }
+        thresholds = new double[T];
         this.T = T;
         this.N = N;
-        this.display = display;
-        failureTracker = new FailureSequence(N);
-        failures = 0;
         runTests();
     }
 
@@ -48,33 +43,14 @@ public class PercolationStats {
         for (int currentTestNumber = 0; currentTestNumber < T; ++currentTestNumber) {
             // initialize required components for test
             Percolation test = new Percolation(N);
-            failureTracker.resetInput();
             // open random sites until the system percolates
             while (!test.percolates()) {
-                draw(test, DEFAULT_DELAY_PAUSE);
                 int randomRow = StdRandom.uniform(N);
                 int randomCol = StdRandom.uniform(N);
-                // record the position of the site opened to facilitate
-                // reproduction of test failure
-                failureTracker.recordInput(randomRow, randomCol);
-                if (test.isOpen(randomRow, randomCol)) {
-                    // handle a failed open operation
-                    if (failureTracker.recordFailedOp() > MAX_FAILED_OPERATIONS) {
-                        failureTracker.recordFailedTest();
-                        failures += 1;
-                        break;
-                    }
-                } else {
-                    // handle a successful open operation
-                    failureTracker.resetFailedOps();
-                }
                 test.open(randomRow, randomCol);
-                draw(test, DEFAULT_DELAY_PAUSE);
             }
             // system has percolated, update thresholds tracker
-            draw(test, DISPLAY_PAUSE_ON_SUCCESS);
-            thresholds[currentTestNumber] = test.numberOfOpenSites();
-            System.out.println("Test " + currentTestNumber + ": " + test.numberOfOpenSites() + " opened sites");
+            thresholds[currentTestNumber] = (double)test.numberOfOpenSites() / (N * N);
         }
     }
 
@@ -89,27 +65,12 @@ public class PercolationStats {
     }
 
     /** Low endpoint of 95% confidence interval. */
-    public double confidenceLow() {
+    public double confidenceLo() {
         return mean() - ((CONFIDENCE_SCALING_FACTOR * stddev()) / Math.sqrt(T));
     }
 
     /** High endpoint of 95% confidence interval. */
-    public double confidenceHigh() {
+    public double confidenceHi() {
         return mean() + ((CONFIDENCE_SCALING_FACTOR * stddev()) / Math.sqrt(T));
-    }
-
-    /** Return the number of failed tests during this run. */
-    public int failures() {
-        return failures;
-    }
-
-    /**
-     * A method to handle interaction with the PercolationVisualizer class to clean up the code a bit.
-     */
-    private void draw(Percolation p, int delay) {
-        if (display) {
-            PercolationVisualizer.draw(p, N);
-            StdDraw.show(delay);
-        }
     }
 }
